cdae71a99daa2d2d025b95c6b192145e175fdd00
Diogo Fagundes <diogo.fagundes-terceiro@cea.com.br>
2024-04-24 13:06:13 -0300

    ci: Fixing files and including mvp for C&A CI

---
+++ src/tests/test.spec.js
@@ -0,0 +1,6 @@
+// Defina aqui seus testes automatizados
+describe('Teste de exemplo', () => {
+    it('should return true when comparing true to true', () => {
+        expect(true).toBe(true);
+    });
+});
